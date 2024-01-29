273fe68f6ad1e78afaa9fbb7feaa3920bbc6aaca
MoeeinAali <Moeeeinaali@gmail.com>
2024-01-29 18:39:37 +0330

    Add Student Class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Student {
+    private static final ArrayList<Student> students = new ArrayList<>();
+    private final String username;
+    private final String password;
+    private final ArrayList<Course> courses;
+
+    private Student(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.courses = new ArrayList<>();
+    }
+    public static Student getStudentByUsername(String username){
+    }
 }
+
