38322958bd87d22e9ad3f68d90f6c5f24c36d0b0
MoeeinAali <Moeeeinaali@gmail.com>
2024-01-29 19:08:58 +0330

    Add MainMenu Class

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -1,4 +1,18 @@
 package view;
 
+import controller.Controller;
+
 public class MainMenu {
+    private Controller controller;
+
+    public MainMenu(Controller controller) {
+        this.controller = controller;
+    }
+
+    public String run() {
+        String command;
+        while (true){
+
+        }
+    }
 }
