c68e0c30c66916535d147a1817552b720aa04cc2
MoeeinAali <Moeeeinaali@gmail.com>
2024-01-29 19:43:24 +0330

    Update LoginMenu

--- src/view/LoginMenu.java
+++ src/view/LoginMenu.java
@@ -2,11 +2,27 @@ package view;
 
 import controller.Controller;
 
+import java.util.regex.Matcher;
+
 public class LoginMenu {
     private Controller controller;
 
     public LoginMenu(Controller controller) {
         this.controller = controller;
     }
+
+    public String run() {
+        Matcher matcher;
+        String command, result;
+        while (true) {
+            command = Menu.getScanner().nextLine();
+            if (command.matches(regexContainer.getRegex("EXIT"))) {
+                return "exit";
+            }
+
+
+        }
+    }
+
 
 }
