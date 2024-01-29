3e47bb17db6e2823e65621f888c44da793ad4c3a
MoeeinAali <Moeeeinaali@gmail.com>
2024-01-29 20:15:00 +0330

    Update Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import controller.Controller;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Controller controller = new Controller();
+        controller.run();
     }
 }
