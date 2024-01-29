b4e9fbfd066b684f69b607df5f53d0c4a5761138
MoeeinAali <Moeeeinaali@gmail.com>
2024-01-29 20:04:05 +0330

    Update TeacherMenu Class

--- src/view/TeacherMenu.java
+++ src/view/TeacherMenu.java
@@ -2,12 +2,34 @@ package view;
 
 import controller.Controller;
 
+import java.util.regex.Matcher;
+
 public class TeacherMenu {
     private Controller controller;
 
     public TeacherMenu(Controller controller) {
         this.controller = controller;
     }
-    
+
+    public void run() {
+        RegexContainer regexContainer = new RegexContainer();
+        Matcher matcher;
+        String command;
+        while (true) {
+            command = Menu.getScanner().nextLine();
+            if (command.matches(regexContainer.getRegex("BACK"))) {
+                return;
+            }
+            if ((matcher = Menu.getMatcher(command, regexContainer.getRegex("ADD_COURSE"))) != null) {
+                System.out.println(controller.addCourse(matcher.group("name"), Integer.parseInt(matcher.group("capacity"))));
+            } else if (command.matches(regexContainer.getRegex("ALL_COURSE"))) {
+                System.out.println(controller.showAllCourse());
+            }
+            else {
+                System.out.println("Invalid Command");
+            }
+        }
+    }
+
 
 }
