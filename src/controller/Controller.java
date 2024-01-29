dc227ed24a29cbcb3ead64bac132aa6860b6f4cf
Moeein Aali <118152946+moeeinaali@users.noreply.github.com>
2024-01-29 18:01:05 +0330

    Add Packages

---
+++ src/controller/Controller.java
@@ -0,0 +1,27 @@
+package controller;
+
+import model.Student;
+import model.Teacher;
+import view.LoginMenu;
+import view.MainMenu;
+import view.StudentMenu;
+import view.TeacherMenu;
+
+public class Controller {
+    private Student loggedInStudent;
+    private Teacher loggedInTeacher;
+    private final LoginMenu loginMenu;
+    private final MainMenu mainMenu;
+    private final TeacherMenu teacherMenu;
+    private final StudentMenu studentMenu;
+
+    public Controller() {
+        loginMenu = new LoginMenu(this);
+        mainMenu = new MainMenu(this);
+        teacherMenu = new TeacherMenu(this);
+        studentMenu = new StudentMenu(this);
+
+    }
+
+}
+
