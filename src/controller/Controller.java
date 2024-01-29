a6ea687abd94a15679c848bf9d56e18e8e8ce11d
MoeeinAali <Moeeeinaali@gmail.com>
2024-01-29 22:22:45 +0330

    Update Controller Class

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -31,7 +31,47 @@ public class Controller {
             return true;
         }
     }
-    public void run(){
+
+    public void run() {
+        if (loginMenu.run().equals("exit")) {
+            return;
+        } else {
+            while (true) {
+                switch (mainMenu.run()) {
+                    case "teacher menu":
+                        teacherMenu.run();
+                        break;
+                    case "student menu":
+                        studentMenu.run();
+                        break;
+                    case "logout":
+                        if (loginMenu.run().equals("exit")) {
+                            return;
+                        }
+                        break;
+                }
+            }
+        }
+
+    }
+
+    public String register(String username, String password, String role) {
+        if (Teacher.getTeacherByUsername(username) != null || Student.getStudentByUsername(username) != null) {
+            return "register failed: username already exists";
+        }
+        if (password.length() < 5 || !password.matches(".*[A-Z].*") || !password.matches(".*[a-z].*") || !password.matches(".*[0-9].*")) {
+            return "register failed: password is weak";
+        }
+        if (role.equals("teacher")) {
+            Teacher.addTeacher(username, password);
+        } else if (role.equals("student")) {
+            Student.addStudent(username, password);
+        } else {
+            return "register failed: invalid role!";
+        }
+        return "register successful";
+    }
+    public String login(String username,String password){
 
     }
 
