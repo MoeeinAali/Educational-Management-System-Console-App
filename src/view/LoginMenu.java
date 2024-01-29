6ecc231651392d5e9d0d83cedcd0efc105ab9495
MoeeinAali <Moeeeinaali@gmail.com>
2024-01-30 00:16:07 +0330

    Edit Regex Checkers

--- src/view/LoginMenu.java
+++ src/view/LoginMenu.java
@@ -12,17 +12,16 @@ public class LoginMenu {
     }
 
     public String run() {
-        RegexContainer regexContainer = new RegexContainer("../Regexes.properties");
         Matcher matcher;
         String command, result;
         while (true) {
             command = Menu.getScanner().nextLine();
-            if (command.matches(regexContainer.getRegex("EXIT"))) {
+            if (command.matches("^\\\\s*exit\\\\s*$")) {
                 return "exit";
             }
-            if ((matcher = Menu.getMatcher(command, regexContainer.getRegex("REGISTER"))) != null) {
+            if ((matcher = Menu.getMatcher(command, "^\\s*register\\s+(?<role>\\S+)\\s+(?<username>\\S+)\\s+(?<password>\\S+)\\s*$")) != null)
                 System.out.println(controller.register(matcher.group("username"), matcher.group("password"), matcher.group("role")));
-            } else if ((matcher = Menu.getMatcher(command, regexContainer.getRegex("LOGIN"))) != null) {
+            else if ((matcher = Menu.getMatcher(command, "^\\s*login\\s+(?<username>\\S+)\\s+(?<password>\\S+)\\s*$")) != null) {
                 result = controller.login(matcher.group("username"), matcher.group("password"));
                 System.out.println(result);
                 if (result.equals("login successful")) {
