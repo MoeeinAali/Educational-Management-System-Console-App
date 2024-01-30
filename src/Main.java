18669953719083f4441aa047dec922ffc948724d
MoeeinAali <Moeeeinaali@gmail.com>
2024-01-30 15:46:02 +0330

    Insert Comments

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import controller.Controller;
 
 public class Main {
     public static void main(String[] args) {
+//        Program Starts with a Controller
         Controller controller = new Controller();
         controller.run();
     }
