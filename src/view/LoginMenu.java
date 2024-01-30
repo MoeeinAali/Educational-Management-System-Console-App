90747f8d904be7c2e575692255f78de70f5e4a24
MoeeinAali <Moeeeinaali@gmail.com>
2024-01-30 11:16:35 +0330

    Fix bugs

--- src/view/LoginMenu.java
+++ src/view/LoginMenu.java
@@ -14,21 +14,22 @@ public class LoginMenu {
     public String run() {
         Matcher matcher;
         String command, result;
+
         while (true) {
             command = Menu.getScanner().nextLine();
-            if (command.matches("^\\\\s*exit\\\\s*$")) {
+
+            if (command.matches("^\\s*exit\\s*$"))
                 return "exit";
-            }
+
             if ((matcher = Menu.getMatcher(command, "^\\s*register\\s+(?<role>\\S+)\\s+(?<username>\\S+)\\s+(?<password>\\S+)\\s*$")) != null)
                 System.out.println(controller.register(matcher.group("username"), matcher.group("password"), matcher.group("role")));
             else if ((matcher = Menu.getMatcher(command, "^\\s*login\\s+(?<username>\\S+)\\s+(?<password>\\S+)\\s*$")) != null) {
                 result = controller.login(matcher.group("username"), matcher.group("password"));
                 System.out.println(result);
-                if (result.equals("login successful")) {
+                if (result.equals("login successful"))
                     return "Logged in";
-                }
             } else {
-                System.out.println("Invalid Command!");
+                System.out.println("invalid command!");
             }
         }
     }
