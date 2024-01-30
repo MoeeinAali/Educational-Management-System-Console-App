4b1a0e20cca2cb1085f0478aa2ee9e80862cd4fb
MoeeinAali <Moeeeinaali@gmail.com>
2024-01-30 11:16:26 +0330

    Fix bugs

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,18 +1,15 @@
 package view;
 
 import java.util.Scanner;
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Menu {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static Matcher getMatcher(String command, String regex) {
         Matcher matcher = Pattern.compile(regex).matcher(command);
-        if (matcher.matches() == true) {
-            return matcher;
-        } else {
-            return null;
-        }
+        return matcher.matches() ? matcher : null;
     }
 
     public static Scanner getScanner() {
